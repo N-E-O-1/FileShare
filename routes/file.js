e33be3a6ae88bef1f82a347e91bf8dddda4a5873
hrishikesh <hrishikale9225@gmail.com>
2022-06-03 19:05:47 +0530

    new

--- routes/file.js
+++ routes/file.js
@@ -47,7 +47,7 @@ router.post('/send',async(req,res) => {
         text:`${emailFrom}shared a file with you`,
         hmtl:require('../services/emailTemplate')({
             emailFrom:emailFrom,
-            downloadLink:`${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            downloadLink:`${process.env.APP_BASE_URL}files/${file.uuid}`,
             size:parseInt(file.size/1000) + ' KB',
             expires:'24 hours'
         })
